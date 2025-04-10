248e740821a6dcfd6db9007795e67d80ec3be75a
JoseLuis-S <shjose@iesrafaelalberti.es>
2025-04-10 21:23:25 +0200

    Creacion .jar hecho

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("es.iesraprog2425.pruebaes.MainKt")
 }
 
 group = "org.prog2425.dam"
@@ -16,6 +22,15 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+    archiveBaseName.set("PruebaCalc")    // Nombre personalizado
+    archiveVersion.set("1.0")                // Versión
+    archiveClassifier.set("")                // Sin sufijo -all
+    mergeServiceFiles()
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA") // Evita errores de firma
+}
+
 kotlin {
     jvmToolchain(21)
 }
